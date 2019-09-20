f46c49ad9a23cf01b56872b0f7f8f413cccf0310
cubdesign <takeo@cubdesign.com>
2019-09-20 23:56:22 +0900

    indexを修正

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,40 @@
 import React from "react"
-import { Link } from "gatsby"
+import {Link} from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Always a new thing. </h1>
-  </Layout>
+    <Layout>
+        <SEO title="Home"/>
+        <h1>Always a new thing. </h1>
+
+        <p>
+            WWD Lead Engineer:<br/>
+            <a href="https://github.com/cubdesign">https://www.wwdjapan.com/</a>
+        </p>
+        <p>
+            Github:<br/>
+            <a href="https://github.com/cubdesign">https://github.com/cubdesign</a>
+        </p>
+        <p>Twitter:<br/>
+            <a href="https://twitter.com/cubdesign">https://twitter.com/cubdesign</a>
+        </p>
+        <p>
+            Facebook:<br/>
+            <a href="https://www.facebook.com/cubdesign">https://www.facebook.com/cubdesign</a>
+        </p>
+        <p>
+            LinkedIn:<br/>
+            <a href="https:/www.linkedin.com/in/takeo-tamura">https:/www.linkedin.com/in/takeo-tamura</a>
+        </p>
+        <p>
+            Instagram:<br/>
+            <a href="https://www.instagram.com/cubdesign">https://www.instagram.com/cubdesign</a>
+        </p>
+
+    </Layout>
 )
 
 export default IndexPage
