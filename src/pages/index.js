6196e7a0f69bcbde95cda8d31041aeb7469083c3
cubdesign <takeo@cubdesign.com>
2020-11-20 15:38:41 +0900

    zenn, qiitaを追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,53 +1,79 @@
 import React from "react"
-import {Link} from "gatsby"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-    <Layout>
-        <SEO title="Home"/>
-        <h1>Always a new thing. </h1>
-        <p>Takeo Tamura（　田村　岳夫　）</p>
+  <Layout>
+    <SEO title="Home" />
+    <h1>Always a new thing. </h1>
+    <p>Takeo Tamura（　田村　岳夫　）</p>
 
-        <p>
-             Lead Engineer @INFAS PUBLICATIONS  Designer, Full-stack Developer. ❤️ 🧘‍️ ❤️ Fashion ❤️ TOKYO ❤️ Serverless ❤️ UX ❤️ WWD ❤️ 🍺 & 🍣
-        </p>
-        <p>
-            WWD Lead Engineer:<br/>
-            <a href="https://www.wwdjapan.com/">https://www.wwdjapan.com/</a>
-        </p>
-        <p>
-            TOKION Lead Engineer:<br/>
-            <a href="https://tokion.jp/en/">https://tokion.jp/en/</a><br />
-            <a href="https://tokion.jp">https://tokion.jp/</a>
-        </p>
-        <p>
-            Github:<br/>
-            <a href="https://github.com/cubdesign">https://github.com/cubdesign</a>
-        </p>
-
-        <p>note:<br/>
-            <a href="https://note.com/cubdesign">https://note.com/cubdesign</a>
-        </p>
-        <p>Twitter:<br/>
-            <a href="https://twitter.com/cubdesign">https://twitter.com/cubdesign</a>
-        </p>
-        <p>
-            Facebook:<br/>
-            <a href="https://www.facebook.com/cubdesign">https://www.facebook.com/cubdesign</a>
-        </p>
-        <p>
-            LinkedIn:<br/>
-            <a href="https://www.linkedin.com/in/takeo-tamura/">https://www.linkedin.com/in/takeo-tamura/</a>
-        </p>
-        <p>
-            Instagram:<br/>
-            <a href="https://www.instagram.com/cubdesign">https://www.instagram.com/cubdesign</a>
-        </p>
-
-    </Layout>
+    <p>
+      休職中。Lead Engineer @INFAS PUBLICATIONS Designer, Full-stack Developer.
+      ❤️ 🧘‍️ ❤️ Fashion ❤️ TOKYO ❤️ Serverless ❤️ UX ❤️ WWD ❤️ 🍺 & 🍣
+    </p>
+    <p>
+      WWD Lead Engineer:
+      <br />
+      <a href="https://www.wwdjapan.com/">https://www.wwdjapan.com/</a>
+    </p>
+    <p>
+      TOKION Lead Engineer:
+      <br />
+      <a href="https://tokion.jp/en/">https://tokion.jp/en/</a>
+      <br />
+      <a href="https://tokion.jp">https://tokion.jp/</a>
+    </p>
+    <p>
+      Github:
+      <br />
+      <a href="https://github.com/cubdesign">https://github.com/cubdesign</a>
+    </p>
+    <p>
+      Twitter:
+      <br />
+      <a href="https://twitter.com/cubdesign">https://twitter.com/cubdesign</a>
+    </p>
+    <p>
+      Facebook:
+      <br />
+      <a href="https://www.facebook.com/cubdesign">
+        https://www.facebook.com/cubdesign
+      </a>
+    </p>
+    <p>
+      Zenn:
+      <br />
+      <a href="https://zenn.dev/cubdesign">https://zenn.dev/cubdesign</a>
+    </p>
+    <p>
+      Qiita:
+      <br />
+      <a href="https://qiita.com/cubdesign">https://qiita.com/cubdesign</a>
+    </p>
+    <p>
+      note:
+      <br />
+      <a href="https://note.com/cubdesign">https://note.com/cubdesign</a>
+    </p>
+    <p>
+      LinkedIn:
+      <br />
+      <a href="https://www.linkedin.com/in/takeo-tamura/">
+        https://www.linkedin.com/in/takeo-tamura/
+      </a>
+    </p>
+    <p>
+      Instagram:
+      <br />
+      <a href="https://www.instagram.com/cubdesign">
+        https://www.instagram.com/cubdesign
+      </a>
+    </p>
+  </Layout>
 )
 
 export default IndexPage
