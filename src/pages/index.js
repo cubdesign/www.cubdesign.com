548fbc2010d4a287357c21bbf9134306e7e67b1d
cubdesign <takeo@cubdesign.com>
2025-11-01 17:31:13 +0900

    remove info

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -12,66 +10,8 @@ const IndexPage = () => (
     <p>Takeo Tamura（　田村　岳夫　）</p>
 
     <p>
-      フリーランス。Engineer, Designer, Full-stack Developer.
-      ❤️ 🧘‍️ ❤️ Fashion ❤️ TOKYO ❤️ Serverless ❤️ UX ❤️ 🍺 & 🍣
-    </p>
-    <p>
-      Github:
-      <br />
-      <a href="https://github.com/cubdesign">https://github.com/cubdesign</a>
-    </p>
-    <p>
-      Twitter:
-      <br />
-      <a href="https://twitter.com/cubdesign">https://twitter.com/cubdesign</a>
-    </p>
-    <p>
-      Facebook:
-      <br />
-      <a href="https://www.facebook.com/cubdesign">
-        https://www.facebook.com/cubdesign
-      </a>
-    </p>
-    <p>
-      Zenn:
-      <br />
-      <a href="https://zenn.dev/cubdesign">https://zenn.dev/cubdesign</a>
-    </p>
-    <p>
-      Qiita:
-      <br />
-      <a href="https://qiita.com/cubdesign">https://qiita.com/cubdesign</a>
-    </p>
-    <p>
-      note:
-      <br />
-      <a href="https://note.com/cubdesign">https://note.com/cubdesign</a>
-    </p>
-    <p>
-      LinkedIn:
-      <br />
-      <a href="https://www.linkedin.com/in/takeo-tamura/">
-        https://www.linkedin.com/in/takeo-tamura/
-      </a>
-    </p>
-    <p>
-      Instagram:
-      <br />
-      <a href="https://www.instagram.com/cubdesign">
-        https://www.instagram.com/cubdesign
-      </a>
-    </p>
-
-    <p>
-      Tech Blog
-      <br />
-      <a href="https://webkei.dev">https://webkei.dev</a>
-    </p>
-
-    <p>
-      Tech Blog 2
-      <br />
-      <a href="https://dev.cubdesign.com">https://dev.cubdesign.com</a>
+      Engineer, Designer, Full-stack Developer.
+      ❤️ 🧘‍️ ❤️ Fashion ❤️ TOKYO ❤️ Serverless ❤️ UX ❤️ 🍶 & 🍣
     </p>
   </Layout>
 )
